83feef14196bb7fcae813fcf62654476f78c727e
Kyle David <kyle.david@redline-electronics.com>
2013-05-29 16:56:08 -0400

    populate list of saved videos

--- src/com/ryanallen/bbox/ReviewFragment.java
+++ src/com/ryanallen/bbox/ReviewFragment.java
@@ -1,16 +1,23 @@
 package com.ryanallen.bbox;
 
+import java.util.ArrayList;
+
 import android.app.ListFragment;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 public class ReviewFragment extends ListFragment {
 	private ArrayAdapter<VideoFile> mArrayAdapter;
+	ArrayList<VideoFile> videoFiles;
 	private ListView reviewListView;
+	
+	private static final String SELECTED_VIDEO_FILE = "SELECTED_VIDEO_FILE";
 	
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -31,6 +38,10 @@ public class ReviewFragment extends ListFragment {
 		super.onViewCreated(view, savedInstanceState);
 		// populate the listview with flagged videos
 		reviewListView = (ListView)getListView();
+		videoFiles = VideoFile.getAllBboxVideos();
+		
+		mArrayAdapter = new ArrayAdapter<VideoFile>(getActivity(), android.R.layout.simple_list_item_1, videoFiles);
+		reviewListView.setAdapter(mArrayAdapter);
 	}
 	
 	@Override
@@ -38,6 +49,13 @@ public class ReviewFragment extends ListFragment {
 	 * Start the playback activity for the selected video
 	 */
 	public void onListItemClick(ListView l, View v, int position, long id) {
+		// get the selected video file
+		VideoFile selectedFile = (VideoFile)l.getItemAtPosition(position);
 		
+		Toast.makeText(getActivity(), selectedFile.getPath(), Toast.LENGTH_SHORT).show();
+		
+//		// setup the playback activity
+//		Intent videoPlaybackIntent = new Intent(getActivity(), PlaybackActivity.class);
+//		videoPlaybackIntent.putExtra(SELECTED_VIDEO_FILE, selectedFile.filePath);
 	}
 }
--- src/com/ryanallen/bbox/VideoFile.java
+++ src/com/ryanallen/bbox/VideoFile.java
@@ -1,9 +1,40 @@
 package com.ryanallen.bbox;
 
-public class VideoFile {
+import java.io.File;
+import java.util.ArrayList;
+import android.os.Environment;
 
-	public VideoFile() {
+public class VideoFile extends File {
+	private static final long serialVersionUID = -6694017648316508704L;
+	public String filePath;
+	
+	public VideoFile(String path) {
+		super(path);
+		filePath = path;
 		// TODO Auto-generated constructor stub
 	}
-
+	
+	public static ArrayList<VideoFile> getAllBboxVideos() {
+		// get the save directory for the black box recordings
+		File videoStorageDir = new File(
+			Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_MOVIES), 
+			"BlackBox");
+		
+		// get a list of files
+		if (videoStorageDir.exists()) {
+			ArrayList<VideoFile> videoFileList = new ArrayList<VideoFile>();
+			File[] fileArray = videoStorageDir.listFiles();
+			for (File file : fileArray) {
+				videoFileList.add(new VideoFile(file.getAbsolutePath()));
+			}
+			return videoFileList;
+		} else {
+			return null;
+		}
+	}
+	
+	@Override
+	public String toString() {
+		return getName();
+	}
 }
