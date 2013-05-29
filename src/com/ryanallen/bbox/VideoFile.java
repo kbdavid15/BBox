9bfe4a8ee5a184802d98a4e1470950a23f326930
Kyle David <kyle.david@redline-electronics.com>
2013-05-29 17:17:08 -0400

    working on a more descriptive view of saved videos

--- src/com/ryanallen/bbox/VideoFile.java
+++ src/com/ryanallen/bbox/VideoFile.java
@@ -2,7 +2,14 @@ package com.ryanallen.bbox;
 
 import java.io.File;
 import java.util.ArrayList;
+
+import android.content.Context;
 import android.os.Environment;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.LinearLayout;
+import android.widget.TwoLineListItem;
 
 public class VideoFile extends File {
 	private static final long serialVersionUID = -6694017648316508704L;
@@ -37,4 +44,47 @@ public class VideoFile extends File {
 	public String toString() {
 		return getName();
 	}
+	
+	class TwoLineAdapter extends BaseAdapter {
+		private Context context;
+		private ArrayList<VideoFile> videoFiles;
+		
+		public TwoLineAdapter(Context context, ArrayList<VideoFile> videoFiles) {
+			this.context = context;
+			this.videoFiles = videoFiles;
+		}
+
+		@Override
+		public int getCount() {
+			return videoFiles.size();
+		}
+		@Override
+		public Object getItem(int position) {
+			return videoFiles.get(position);
+		}
+		@Override
+		public long getItemId(int position) {
+			return 0;
+		}
+		@Override
+		public View getView(int position, View convertView, ViewGroup parent) {
+			LinearLayout twoLineLayout;
+			
+			if (convertView == null) {
+				twoLineLayout = new LinearLayout(context);
+				LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.FILL_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+				//TODO make a template in xml and use layout inflater
+			} else {
+				twoLineLayout = (LinearLayout)convertView;
+			}
+			
+			return twoLineLayout;
+		}
+	}
+	
+	
+	
+	
+	
+	
 }
