44784822ed988f3d81c6e1695346ea3481b513bf
Kyle David <kyle.david@redline-electronics.com>
2013-05-30 01:31:34 -0400

    added google maps api key

---
+++ src/com/ryanallen/bbox/LocationTracker.java
@@ -0,0 +1,11 @@
+package com.ryanallen.bbox;
+
+public class LocationTracker {
+
+	public LocationTracker() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	// code for checking if Google Play services is enabled
+
+}
