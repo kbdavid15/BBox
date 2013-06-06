2ca337516b1e44d06766eccf3c9796eae3b2f0f6
Kyle David <kyle.david@redline-electronics.com>
2013-06-06 04:06:39 -0400

    requesting location updates

--- src/com/ryanallen/bbox/MyDbOpenHelper.java
+++ src/com/ryanallen/bbox/MyDbOpenHelper.java
@@ -7,16 +7,24 @@ import android.database.sqlite.SQLiteOpenHelper;
 public class MyDbOpenHelper extends SQLiteOpenHelper {
 	private static final String DB_NAME = "locations.db";
 	private static final int DB_VERSION = 1;
+	
+	private static final String CREATE_TABLE = 
+			"CREATE TABLE IF NOT EXISTS `videolocation` (" +
+			  "`id` int(10) unsigned NOT NULL AUTO_INCREMENT, " +
+			  "`filename` varchar(25) NOT NULL," +
+			  "`latitude` double NOT NULL," +
+			  "`longitude` double NOT NULL," +
+			  "`time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP," +
+			  "PRIMARY KEY (`id`)," +
+			  "KEY `filename` (`filename`));";
 
 	public MyDbOpenHelper(Context context) {
 		super(context, DB_NAME, null, DB_VERSION);
-		// TODO Auto-generated constructor stub
 	}
 
 	@Override
 	public void onCreate(SQLiteDatabase db) {
-		// TODO Auto-generated method stub
-
+		db.execSQL(CREATE_TABLE);
 	}
 
 	@Override
--- src/com/ryanallen/bbox/RecordingFullscreenActivity.java
+++ src/com/ryanallen/bbox/RecordingFullscreenActivity.java
@@ -2,19 +2,29 @@ package com.ryanallen.bbox;
 
 import java.io.IOException;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesClient;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.location.LocationClient;
+import com.google.android.gms.location.LocationRequest;
 import com.ryanallen.bbox.util.SystemUiHider;
 
 import android.annotation.TargetApi;
 import android.app.Activity;
+import android.app.Dialog;
+import android.app.DialogFragment;
 import android.content.Intent;
+import android.content.IntentSender;
 import android.content.SharedPreferences;
 import android.hardware.Camera;
+import android.location.Location;
 import android.media.CamcorderProfile;
 import android.media.MediaRecorder;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.preference.PreferenceManager;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.MenuItem;
@@ -29,7 +39,9 @@ import android.support.v4.app.NavUtils;
  * 
  * @see SystemUiHider
  */
-public class RecordingFullscreenActivity extends Activity {
+public class RecordingFullscreenActivity extends Activity implements GooglePlayServicesClient.ConnectionCallbacks, 
+																	 GooglePlayServicesClient.OnConnectionFailedListener,
+																	 com.google.android.gms.location.LocationListener {
 	/**
 	 * Whether or not the system UI should be auto-hidden after
 	 * {@link #AUTO_HIDE_DELAY_MILLIS} milliseconds.
@@ -57,12 +69,21 @@ public class RecordingFullscreenActivity extends Activity {
 	 * The instance of the {@link SystemUiHider} for this activity.
 	 */
 	private SystemUiHider mSystemUiHider;
-	
+
 	private Camera myCamera;
 	private CameraPreview mPreview;
 	private FrameLayout mFrameLayoutPreview;
 	private MediaRecorder mMediaRecorder;
 	private SharedPreferences settings;
+	
+	private ConnectionResult connectionResult;
+	public static final int LOCATION_UPDATE_INTERVAL = 5000;
+	// The fastest update frequency, in milliseconds
+    private static final int FASTEST_INTERVAL = 1000;
+    // Define an object that holds accuracy and frequency parameters
+    LocationRequest mLocationRequest;
+    LocationClient mLocationClient;
+    boolean mUpdatesRequested;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -71,6 +92,22 @@ public class RecordingFullscreenActivity extends Activity {
 		setContentView(R.layout.activity_recording_fullscreen);
 		setupActionBar();
 		
+		// location stuff
+		mLocationRequest = LocationRequest.create();
+		// Use high accuracy
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        // Set the update interval to 5 seconds
+        mLocationRequest.setInterval(LOCATION_UPDATE_INTERVAL);
+        // Set the fastest update interval to 1 second
+        mLocationRequest.setFastestInterval(FASTEST_INTERVAL);
+        //TODO use location update freq from settings
+        
+        /*
+         * Create a new location client, using the enclosing class to
+         * handle callbacks.
+         */
+        mLocationClient = new LocationClient(this,this,this);
+
 		// restore prefs
 		settings = PreferenceManager.getDefaultSharedPreferences(this);
 
@@ -83,56 +120,56 @@ public class RecordingFullscreenActivity extends Activity {
 				HIDER_FLAGS);
 		mSystemUiHider.setup();
 		mSystemUiHider
-				.setOnVisibilityChangeListener(new SystemUiHider.OnVisibilityChangeListener() {
-					// Cached values.
-					int mControlsHeight;
-					int mShortAnimTime;
+		.setOnVisibilityChangeListener(new SystemUiHider.OnVisibilityChangeListener() {
+			// Cached values.
+			int mControlsHeight;
+			int mShortAnimTime;
 
-					@Override
-					@TargetApi(Build.VERSION_CODES.HONEYCOMB_MR2)
-					public void onVisibilityChange(boolean visible) {
-						if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB_MR2) {
-							// If the ViewPropertyAnimator API is available
-							// (Honeycomb MR2 and later), use it to animate the
-							// in-layout UI controls at the bottom of the
-							// screen.
-							if (mControlsHeight == 0) {
-								mControlsHeight = controlsView.getHeight();
-							}
-							if (mShortAnimTime == 0) {
-								mShortAnimTime = getResources().getInteger(
-										android.R.integer.config_shortAnimTime);
-							}
-							controlsView
-									.animate()
-									.translationY(visible ? 0 : mControlsHeight)
-									.setDuration(mShortAnimTime);
-						} else {
-							// If the ViewPropertyAnimator APIs aren't
-							// available, simply show or hide the in-layout UI
-							// controls.
-							controlsView.setVisibility(visible ? View.VISIBLE
-									: View.GONE);
-						}
-
-						if (visible && AUTO_HIDE) {
-							// Schedule a hide().
-							delayedHide(AUTO_HIDE_DELAY_MILLIS);
-						}
+			@Override
+			@TargetApi(Build.VERSION_CODES.HONEYCOMB_MR2)
+			public void onVisibilityChange(boolean visible) {
+				if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB_MR2) {
+					// If the ViewPropertyAnimator API is available
+					// (Honeycomb MR2 and later), use it to animate the
+					// in-layout UI controls at the bottom of the
+					// screen.
+					if (mControlsHeight == 0) {
+						mControlsHeight = controlsView.getHeight();
 					}
-				});
+					if (mShortAnimTime == 0) {
+						mShortAnimTime = getResources().getInteger(
+								android.R.integer.config_shortAnimTime);
+					}
+					controlsView
+					.animate()
+					.translationY(visible ? 0 : mControlsHeight)
+					.setDuration(mShortAnimTime);
+				} else {
+					// If the ViewPropertyAnimator APIs aren't
+					// available, simply show or hide the in-layout UI
+					// controls.
+					controlsView.setVisibility(visible ? View.VISIBLE
+							: View.GONE);
+				}
+
+				if (visible && AUTO_HIDE) {
+					// Schedule a hide().
+					delayedHide(AUTO_HIDE_DELAY_MILLIS);
+				}
+			}
+		});
 
 		// Set up the user interaction to manually show or hide the system UI.
-//		contentView.setOnClickListener(new View.OnClickListener() {
-//			@Override
-//			public void onClick(View view) {
-//				if (TOGGLE_ON_CLICK) {
-//					mSystemUiHider.toggle();
-//				} else {
-//					mSystemUiHider.show();
-//				}
-//			}
-//		});
+		//		contentView.setOnClickListener(new View.OnClickListener() {
+		//			@Override
+		//			public void onClick(View view) {
+		//				if (TOGGLE_ON_CLICK) {
+		//					mSystemUiHider.toggle();
+		//				} else {
+		//					mSystemUiHider.show();
+		//				}
+		//			}
+		//		});
 
 		// Upon interacting with UI controls, delay any scheduled hide()
 		// operations to prevent the jarring behavior of controls going away
@@ -150,26 +187,32 @@ public class RecordingFullscreenActivity extends Activity {
 		// are available.
 		delayedHide(100);
 	}
-	
+
 	@Override
 	public void onResume() {
 		super.onResume();
-		
+
 		// start the Location Tracking service
 		startService(new Intent(this, LocationTrackerService.class));
-		
+
 		// Get the Camera instance as the activity achieves full user focus
-	    if (myCamera == null) {
-	        initializeCamera(); // Local method to handle camera init
-	    }
+		if (myCamera == null) {
+			initializeCamera(); // Local method to handle camera init
+		}
+	}
+	
+	@Override
+	protected void onStart() {
+		super.onStart();
+		mLocationClient.connect();
 	}
 	
 	private boolean initializeCamera() {
 		// create an instance of camera
 		myCamera = getCameraInstance();
-		
+
 		if (myCamera == null) return false;
-		
+
 		if (mPreview == null) {
 			mPreview = new CameraPreview(this, myCamera);
 			mFrameLayoutPreview = (FrameLayout)findViewById(R.id.test);
@@ -179,22 +222,24 @@ public class RecordingFullscreenActivity extends Activity {
 	}
 
 	@Override
-	public void onPause() {
-	    super.onPause();  // Always call the superclass method first
-	    
-	    // Release the Camera because we don't need it when paused
-	    // and other activities might need to use it.
-	    if (myCamera != null) {
-	    	myCamera.release();
-	        myCamera = null;
-	    }
+	protected void onPause() {
+		super.onPause();  // Always call the superclass method first
+
+		// Release the Camera because we don't need it when paused
+		// and other activities might need to use it.
+		if (myCamera != null) {
+			myCamera.release();
+			myCamera = null;
+		}
 	}
 	
+	
+
 	public void captureButton_Click(View v) {
-	    if (((ToggleButton)v).isChecked()) {	    	
-	    	// start recording
-	    	mMediaRecorder = new MediaRecorder();
-	    	myCamera.unlock();			
+		if (((ToggleButton)v).isChecked()) {	    	
+			// start recording
+			mMediaRecorder = new MediaRecorder();
+			myCamera.unlock();			
 			mMediaRecorder.setCamera(myCamera);
 			mMediaRecorder.setAudioSource(MediaRecorder.AudioSource.CAMCORDER);
 			mMediaRecorder.setVideoSource(MediaRecorder.VideoSource.CAMERA);
@@ -209,10 +254,10 @@ public class RecordingFullscreenActivity extends Activity {
 				Toast.makeText(this, "The selected video quality is not available. Using highest available quality.", Toast.LENGTH_LONG).show();
 				mMediaRecorder.setProfile(CamcorderProfile.get(CamcorderProfile.QUALITY_HIGH));
 			}
-			
+
 			mMediaRecorder.setOutputFile(Media.getOutputMediaFile(Media.MEDIA_TYPE_VIDEO).toString());
 			mMediaRecorder.setPreviewDisplay(mPreview.getHolder().getSurface());
-			
+
 			// prepare the media recorder
 			try {
 				mMediaRecorder.prepare();
@@ -224,26 +269,26 @@ public class RecordingFullscreenActivity extends Activity {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-	    } else {
-	    	// stop recording
-	    	mMediaRecorder.stop();
-	    	mMediaRecorder.reset();
-	    	mMediaRecorder.release();
-	    	myCamera.lock();	    	
-	    }
+		} else {
+			// stop recording
+			mMediaRecorder.stop();
+			mMediaRecorder.reset();
+			mMediaRecorder.release();
+			myCamera.lock();	    	
+		}
 	}
-	
-	
+
+
 	/** A safe way to get an instance of the Camera object. */
 	public static Camera getCameraInstance() {
-	    Camera c = null;
-	    try {
-	        c = Camera.open(); // attempt to get a Camera instance
-	    }
-	    catch (Exception e){
-	        // Camera is not available (in use or does not exist)
-	    }
-	    return c; // returns null if camera is unavailable
+		Camera c = null;
+		try {
+			c = Camera.open(); // attempt to get a Camera instance
+		}
+		catch (Exception e){
+			// Camera is not available (in use or does not exist)
+		}
+		return c; // returns null if camera is unavailable
 	}
 
 	/**
@@ -307,4 +352,167 @@ public class RecordingFullscreenActivity extends Activity {
 		mHideHandler.removeCallbacks(mHideRunnable);
 		mHideHandler.postDelayed(mHideRunnable, delayMillis);
 	}
+	/**
+     * Called by Location Services if the attempt to
+     * Location Services fails.
+     */
+	@Override
+	public void onConnectionFailed(ConnectionResult connectionResult) {
+		this.connectionResult = connectionResult;
+		/*
+         * Google Play services can resolve some errors it detects.
+         * If the error has a resolution, try sending an Intent to
+         * start a Google Play services activity that can resolve
+         * error.
+         */
+        if (connectionResult.hasResolution()) {
+            try {
+                // Start an Activity that tries to resolve the error
+                connectionResult.startResolutionForResult(
+                        this,
+                        CONNECTION_FAILURE_RESOLUTION_REQUEST);
+                /*
+                * Thrown if Google Play services canceled the original
+                * PendingIntent
+                */
+            } catch (IntentSender.SendIntentException e) {
+                // Log the error
+                e.printStackTrace();
+            }
+        } else {
+            /*
+             * If no resolution is available, display a dialog to the
+             * user with the error.
+             */
+            showDialog(connectionResult.getErrorCode());
+        }
+
+	}
+	/**
+     * Called by Location Services when the request to connect the
+     * client finishes successfully. At this point, you can
+     * request the current location or start periodic updates
+     */
+	@Override
+	public void onConnected(Bundle dataBundle) {
+        mLocationClient.requestLocationUpdates(mLocationRequest, this);
+	}
+	/**
+     * Called by Location Services if the connection to the
+     * location client drops because of an error.
+     */
+	@Override
+	public void onDisconnected() {
+		// Display the connection status
+        Toast.makeText(this, "Disconnected. Please re-connect.", Toast.LENGTH_SHORT).show();
+	}
+
+	@Override
+	public void onLocationChanged(Location location) {
+		// Report to the UI that the location was updated
+        String msg = "Updated Location: " +
+                Double.toString(location.getLatitude()) + "," +
+                Double.toString(location.getLongitude());
+        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
+	}
+	
+	@Override
+	protected void onStop() {
+		// If the client is connected
+        if (mLocationClient.isConnected()) {
+            /*
+             * Remove location updates for a listener.
+             * The current Activity is the listener, so
+             * the argument is "this".
+             */
+            mLocationClient.removeLocationUpdates(this);
+        }
+        /*
+         * After disconnect() is called, the client is
+         * considered "dead".
+         */
+        mLocationClient.disconnect();
+        super.onStop();
+	}
+
+	/*
+	 * Define a request code to send to Google Play services
+	 * This code is returned in Activity.onActivityResult
+	 */
+	private final static int CONNECTION_FAILURE_RESOLUTION_REQUEST = 9000;
+	// Define a DialogFragment that displays the error dialog
+	public static class ErrorDialogFragment extends DialogFragment {
+		// Global field to contain the error dialog
+		private Dialog mDialog;
+		// Default constructor. Sets the dialog field to null
+		public ErrorDialogFragment() {
+			super();
+			mDialog = null;
+		}
+		// Set the dialog to display
+		public void setDialog(Dialog dialog) {
+			mDialog = dialog;
+		}
+		// Return a Dialog to the DialogFragment.
+		@Override
+		public Dialog onCreateDialog(Bundle savedInstanceState) {
+			return mDialog;
+		}
+	}
+
+	@Override
+	public void onActivityResult(int requestCode, int resultCode, Intent data) {
+		// Decide what to do based on the original request code
+		switch (requestCode) {
+		case CONNECTION_FAILURE_RESOLUTION_REQUEST :
+			/*
+			 * If the result code is Activity.RESULT_OK, try
+			 * to connect again
+			 */
+			switch (resultCode) {
+			case Activity.RESULT_OK :
+				/*
+				 * Try the request again
+				 */
+				break;
+			}
+		}
+	}
+	
+	private boolean servicesConnected() {
+        // Check that Google Play services is available
+        int resultCode =
+                GooglePlayServicesUtil.
+                        isGooglePlayServicesAvailable(this);
+        // If Google Play services is available
+        if (ConnectionResult.SUCCESS == resultCode) {
+            // In debug mode, log the status
+            Log.d("Location Updates",
+                    "Google Play services is available.");
+            // Continue
+            return true;
+        // Google Play services was not available for some reason
+        } else {
+            // Get the error code
+            int errorCode = connectionResult.getErrorCode();
+            // Get the error dialog from Google Play services
+            Dialog errorDialog = GooglePlayServicesUtil.getErrorDialog(
+                    errorCode,
+                    this,
+                    CONNECTION_FAILURE_RESOLUTION_REQUEST);
+            // If Google Play services can provide an error dialog
+            if (errorDialog != null) {
+                // Create a new DialogFragment for the error dialog
+                ErrorDialogFragment errorFragment =
+                        new ErrorDialogFragment();
+                // Set the dialog in the DialogFragment
+                errorFragment.setDialog(errorDialog);
+                // Show the error dialog in the DialogFragment
+                errorFragment.show(
+                        getFragmentManager(),
+                        "Location Updates");
+            }
+            return false;
+        }
+    }
 }
