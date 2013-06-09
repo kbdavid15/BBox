1cc8e94c0b37632b33fa07e73042ef30947481f0
Kyle David <kyle.david@redline-electronics.com>
2013-06-09 18:46:48 -0400

    added video map fragment sub class

---
+++ src/com/ryanallen/bbox/VideoMapFragment.java
@@ -0,0 +1,18 @@
+package com.ryanallen.bbox;
+
+import com.google.android.gms.maps.MapFragment;
+
+/**
+ * @author Kyle
+ *
+ */
+public class VideoMapFragment extends MapFragment {
+
+	/**
+	 * 
+	 */
+	public VideoMapFragment() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
